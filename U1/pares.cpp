5203ca710bed6208b6384836f61fb051a286ad3c
UP200782 <up200782@alumnos.upa.edu.mx>
2022-10-06 20:02:00 -0500

    Falso

---
+++ U1/pares.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const* argv[])
+{
+    cout << "Pares, o impares" << endl;
+    "\n";
+    cout << "Escriba el numero: " << endl;
+    int n;
+    cin >> n;
+    if (n % 2 == 0)
+    {
+        cout << "Numero par" << endl;
+    }
+    else
+    {
+        cout << "Numero impar" << endl;
+    }
+    return 0;
+}
+// Jorge Antonio Ruiz Esparza Galván, UP200782
